6e22c6e07684204832cee6224dfb9754ad642a93
SaiPreethi28 <preethisai341@gmail.com>
2025-07-11 16:21:00 +0530

    Add files via upload

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
-def home(request):
-    return HttpResponse("Hello, Django!")
+def demo(request):
+    temp=loader.get_template('grid2.html')
+    return HttpResponse(temp.render())
