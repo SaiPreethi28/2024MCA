9e8dd4a40631dc18b5e97916b291d6fb69ed41c1
SaiPreethi28 <preethisai341@gmail.com>
2025-07-13 15:42:44 +0530

    Add files via upload

--- task1/ecommerce/catalog/admin.py
+++ task1/ecommerce/catalog/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
-admin.site.register(Product)
+admin.site.register(Category)
--- task1/ecommerce/catalog/views.py
+++ task1/ecommerce/catalog/views.py
@@ -3,13 +3,44 @@ from django.shortcuts import render
 # Create your views here.
  
 from django.shortcuts import render, get_object_or_404
-from .models import Product
-
+from .models import Product,Category
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login
 
 def product_list(request):
-    ob_products = Product.objects.all()
-    return render(request, 'product_list.html', {'products': ob_products})
+    category_id = request.GET.get('category')
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'product_list.html', {'products': products, 'categories': categories})
 
 def product_detail(request, pk):
     ob_product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_detail.html', {'product': ob_product})
+  
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('product_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def add_to_cart(request, pk):
+    cart = request.session.get('cart', [])
+    if pk not in cart:
+        cart.append(pk)
+    request.session['cart'] = cart
+    return redirect('product_list')
+
+def view_cart(request):
+    cart = request.session.get('cart', [])
+    products = Product.objects.filter(pk__in=cart)
+    return render(request, 'catalog/cart.html', {'products': products})
